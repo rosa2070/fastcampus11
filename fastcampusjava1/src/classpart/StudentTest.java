0bf11bdf831eea9c383719461603884d56fa9648
jaeeunhyun <rosa2070@naver.com>
2020-12-25 13:19:58 +0900

    3rd3rd

--- fastcampusjava1/src/classpart/StudentTest.java
+++ fastcampusjava1/src/classpart/StudentTest.java
@@ -9,6 +9,15 @@ public class StudentTest {
 		studentLee.address = "서울";
 		
 		studentLee.showStudentInfo();
+		
+		Student studentKim = new Student();
+		studentKim.studentName = "김유신";
+		studentKim.address = "경주";
+		
+		studentKim.showStudentInfo();
+		
+		System.out.println(studentLee);
+		System.out.println(studentKim);				
 	}
 
 }
